f6a6f49838b1e0e64fd96b111d468241a2e7519b
metin.kun <metin.kun@192.168.1.103>
2022-01-28 08:09:47 +0300

    added test classes

---
+++ buybaseapi.test/ContactsControllerTest.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using buybaseapi.Data;
+using buybaseapi.Controllers;
+using buybaseapi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace buybaseapi.test
+{
+    public class ContactsControllerTest
+    {
+        private readonly ContactsController _controller;
+        private readonly IContactData _contactData;
+        public ContactsControllerTest()
+        {
+            _contactData = new ContactsFake();
+            _controller = new ContactsController(_contactData);
+        }
+
+        [Fact]
+        public void GetContacts_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.GetContacts();
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+        }
+
+        [Fact]
+        public void GetContacts_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.GetContacts() as OkObjectResult;
+            // Assert
+            var items = Assert.IsType<List<Contact>>(okResult.Value);
+            Assert.Equal(3, items.Count);
+        }
+
+
+        [Fact]
+        public void GetContact_UnknownGuidPassed_ReturnsNotFoundResult()
+        {
+            // Act
+            var notFoundResult = _controller.GetContact(Guid.NewGuid());
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult);
+        }
+
+        [Fact]
+        public void GetContact_ExistingGuidPassed_ReturnsOkResult()
+        {
+            // Arrange
+            var testGuid = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+            // Act
+            var okResult = _controller.GetContact(testGuid);
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+        }
+
+        [Fact]
+        public void GetContact_ExistingGuidPassed_ReturnsRightItem()
+        {
+            // Arrange
+            var testGuid = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+            // Act
+            var okResult = _controller.GetContact(testGuid) as OkObjectResult;
+            // Assert
+            Assert.IsType<Contact>(okResult.Value);
+            Assert.Equal(testGuid, (okResult.Value as Contact).Id);
+        }
+
+        [Fact]
+        public void AddContacts_InvalidObjectPassed_ReturnsBadRequest()
+        {
+            // Arrange
+            var firstNameMissingItem = new Contact()
+            {
+                Firm = "test firm3",
+                LastName = "atac"
+            };
+            _controller.ModelState.AddModelError("FirstName", "Required");
+            // Act
+            var badResponse = _controller.AddContacts(firstNameMissingItem);
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+
+        [Fact]
+        public void AddContacts_ValidObjectPassed_ReturnsCreatedResponse()
+        {
+            // Arrange
+            Contact testItem = new Contact()
+            {
+                FirstName = "Ahmet",
+                LastName = "Guri",
+                Firm = "avax"
+            };
+            // Act
+            var createdResponse = _controller.AddContacts(testItem);
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(createdResponse);
+        }
+
+        [Fact]
+        public void AddContacts_ValidObjectPassed_ReturnedResponseHasCreatedItem()
+        {
+            // Arrange
+            var testItem = new Contact()
+            {
+                FirstName = "Ahmet",
+                LastName = "Guri",
+                Firm = "avax"
+            };
+            // Act
+            var createdResponse = _controller.AddContacts(testItem) as CreatedAtActionResult;
+            var item = createdResponse.Value as Contact;
+            // Assert
+            Assert.IsType<Contact>(item);
+            Assert.Equal("Ahmet", item.FirstName);
+        }
+
+        [Fact]
+        public void DeleteContact_NotExistingGuidPassed_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var notExistingGuid = Guid.NewGuid();
+            // Act
+            var badResponse = _controller.DeleteContact(notExistingGuid);
+            // Assert
+            Assert.IsType<NotFoundResult>(badResponse as NotFoundResult);
+        }
+
+        [Fact]
+        public void DeleteContact_ExistingGuidPassed_ReturnsNoContentResult()
+        {
+            // Arrange
+            var existingGuid = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+            // Act
+            var noContentResponse = _controller.DeleteContact(existingGuid);
+            // Assert
+            Assert.IsType<OkResult>(noContentResponse);
+        }
+
+        [Fact]
+        public void Remove_ExistingGuidPassed_RemovesOneItem()
+        {
+            // Arrange
+            var existingGuid = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+            // Act
+            var okResponse = _controller.DeleteContact(existingGuid);
+            // Assert
+            Assert.Equal(2, _contactData.GetContacts().Count());
+        }
+
+
+    }
+}
---
+++ buybaseapi.test/ContactsFake.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using buybaseapi.Data;
+using buybaseapi.Models;
+
+namespace buybaseapi.test
+{
+    public class ContactsFake : IContactData
+    {
+        readonly List<Contact> _contacts;
+        public ContactsFake()
+        {
+            _contacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                    FirstName = "Metin",
+                    LastName = "Kun",
+                    Firm = "Test firm1"
+                },
+                new Contact()
+                {
+                    Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
+                    FirstName = "Cetin",
+                    LastName = "Kun",
+                    Firm = "Test firm2"
+                },
+                new Contact()
+                {
+                    Id = new Guid("33704c4a-5b87-464c-bfb6-51971b4d18ad"),
+                    FirstName = "ali",
+                    LastName = "Kun",
+                    Firm = "Test firm2"
+                },
+            };
+        } 
+
+        public Contact AddContact(Contact contact)
+        {
+            contact.Id = Guid.NewGuid();
+            _contacts.Add(contact);
+            return contact;
+        }
+
+        public void DeleteContact(Contact contact)
+        {
+            var existing = _contacts.First(a => a.Id == contact.Id);
+            _contacts.Remove(existing);
+        }
+
+        public Contact EditContact(Contact contact)
+        {
+            var existingContact = _contacts.Where(a => a.Id == contact.Id)
+            .FirstOrDefault();
+            existingContact.FirstName = contact.FirstName;
+            existingContact.LastName = contact.LastName;
+            existingContact.Firm = contact.Firm;
+            return existingContact;
+        }
+
+        public Contact GetContact(Guid id)
+        {
+            return _contacts.Where(a => a.Id == id)
+            .FirstOrDefault();
+        }
+
+        public List<Contact> GetContacts()
+        {
+            return _contacts;
+        }
+    }
+}
